84a85b9581b43fde0ca34655f91a1286ab373b9b
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-27 21:52:52 +0530

    API

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { Provider } from "react-redux";
 import store from "./Strore/Store";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./Components/NavBar";
-export const url = "local";
 export const green = "#34D399";
+// export const url = "http://localhost:4500";
+export const url = "https://employeedashboard-server.onrender.com";
 function App() {
   return (
     <>
